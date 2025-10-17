c280a7545afb8ea1ff36a3bbc6db045368897e76
gagg0ti <zahidniazi@gmail.com>
2025-10-17 03:10:09 -0400

    Added order function

--- midterm_project_Zahid_Khan.py
+++ midterm_project_Zahid_Khan.py
@@ -8,10 +8,32 @@ if len(sys.argv) != 2:
 orders_file = sys.argv[1]
 
 try:
-    with open(orders_file, 'r') as f:
-        data = json.load(f)
+    with open(orders_file, "r") as f:
+        orders = json.load(f)
 except Exception as e:
     print(e)
     sys.exit(1)
+
+def phone_num_validation(phone):
+    # Accept anything that can be turned into a string, then validate
+    parts = str(phone).split('-')
+    if len(parts) != 3:
+        return False
+    if parts[0].isdigit() and parts[1].isdigit() and parts[2].isdigit() and len(parts[0]) == 3 and len(parts[1]) == 3 and len(parts[2]) == 4:
+        return  True
+    else:
+        return False
+
+customers = {}  # phone -> name
+items = {}      # item_name -> {"price": number, "orders": number}
+
+for order in orders:
+    # customers
+    name = order.get("name")
+    phone = order.get("phone")
+    if phone_num_validation(phone):
+        customers[phone] = name
+
+print(customers)
 
 print("END")
