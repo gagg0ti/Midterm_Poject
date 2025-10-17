c2e0c5396ef79eeb795373a79ba182484fbae8e0
gagg0ti <zahidniazi@gmail.com>
2025-10-17 01:42:40 -0400

    Setting File Parameter

--- midterm_project_Zahid_Khan.py
+++ midterm_project_Zahid_Khan.py
@@ -5,3 +5,13 @@ if len(sys.argv) != 2:
     print("Provide path to json file ")
     sys.exit(1)
 
+orders_file = sys.argv[1]
+
+try:
+    with open(orders_file, 'r') as f:
+        data = json.load(f)
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+print("END")
